57c6b2f431a747e9bad0bced1e7d4e35c69bc4ca
Geoffrey J. Teale <tealeg@gmail.com>
2017-05-17 23:24:06 +0200

    Expand unit test coverage

--- xlnumfmt_test.go
+++ xlnumfmt_test.go
@@ -1,7 +1,9 @@
 package xlnumfmt
 
 import (
+	"bytes"
 	"testing"
+
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,4 +12,40 @@ func TestIsWhiteSpace(t *testing.T) {
 	assert.True(t, isWhiteSpace('\t'))
 	assert.True(t, isWhiteSpace('\n'))
 	assert.False(t, isWhiteSpace('a'))
+}
+
+func TestIsScientificStartChar(t *testing.T) {
+	assert.True(t, isScientificStartChar('e'))
+	assert.True(t, isScientificStartChar('E'))
+	assert.False(t, isScientificStartChar('f'))
+	assert.False(t, isScientificStartChar('F'))
+}
+
+func TestIsScientificModifier(t *testing.T) {
+	assert.True(t, isScientificModifier('+'))
+	assert.True(t, isScientificModifier('-'))
+	assert.True(t, isScientificModifier('0'))
+}
+
+func TestIsDigit(t *testing.T) {
+	assert.True(t, isDigit('0'))
+
+	// This is perculiar to the Excel NumFmt syntax - only 0 is a
+	// valid digit.
+	assert.False(t, isDigit('1'))
+
+	assert.False(t, isDigit('a'))
+}
+
+// NewScanner returns a pointer to a newly allocated Scanner that
+// wraps the provided io.Reader.
+func TestNewScanner(t *testing.T) {
+	scanner := NewScanner(bytes.NewBufferString("foo"))
+	assert.NotNil(t, scanner)
+	assert.IsType(t, Scanner{}, *scanner)
+	result := make([]byte, 3, 3)
+	count, err := scanner.r.Read(result)
+	assert.Nil(t, err)
+	assert.Equal(t, 3, count)
+	assert.Equal(t, []byte{'f', 'o', 'o'}, result)
 }
